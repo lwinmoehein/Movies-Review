0ab5f57f6aaaf8e3a71d071bffad5db8c958d214
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-01-02 15:36:26 +0630

    - adjust search ui

--- resources/views/admin/movies/index.blade.php
+++ resources/views/admin/movies/index.blade.php
@@ -7,12 +7,9 @@
         @csrf
     <section class="add-section">
         <div class="search-box-wrapper">
-
             <input type="text" name="queryString" value="{{old('queryString')??''}}" placeholder="Search by Code or Title">
             <button type="submit" aria-label="submit form">
-                <svg aria-hidden="true">
-                    <use xlink:href="#search"></use>
-                </svg>
+                <i class="fa fa-search"></i>
             </button>
         </div>
         <button class="btn add-btn"><a href="{{route('movie.create')}}">Create New Movie<i class="ml-2 fa fa-plus"></i></a></button>
